a09e7aaa1dea94a0ed9279ca822ece76cafe445a
CristianDagla <cdaglaroglu@lasalleflorida.edu.ar>
2024-04-10 09:20:00 -0300

    aplicando cambios finales

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path'); // Import path module
 
+const bodyParser = require('body-parser');
+
 const app = express();
 const port = 3000;
 
@@ -32,14 +34,34 @@ app.get('/players', async (req, res) => {
 })
 
 
-app.get('/mangutiorotador', async (req, res) => {
+app.get('/errortry', async (req, res) => {
   try{
-    
+    alert(variable_no_existente); //Con esta variable hacemos que exista un error
+    res.status(200).send({"Mensaje": "No hay un error en el Try"});
     }catch(e){
-      res.status(500).send({'error': 'Maguito rotador de antonic roto...'})
+      res.status(500).send({'Error': 'Hay un error en el Try'})
     }
 })
 
+app.put('/pedrito', async (req, res) => {
+  try{
+    //alert(variable_no_existente); Comentando esta variable hacemos que no exista ningun error
+    res.status(200).send({"Mensaje": "Todo correcto"});
+    }catch(e){
+      res.status(500).send({'Error': 'No correcto'})
+    }
+})
+
+
+app.post('/marcelito', async (req, res) => {
+  try{
+    res.status(200).send({
+      "recibi": req.body
+    })
+  }catch(e){
+    res.status(500).send({'error': 'Internal server error'})
+  }
+})  
 
 app.get('/boom', async (req, res) => {
   res.status(500).json({ message: "My bad" })
